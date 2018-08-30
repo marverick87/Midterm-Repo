31e16237699756f2d72c162d3afc55b0b739740a
marverick87 <marverick87@yahoo.com>
2018-08-30 15:33:34 +0800

    Navarro,Marverick/8/23/18

--- Seatwork#1/Sw1.R
+++ Seatwork#1/Sw1.R
@@ -1,21 +1,23 @@
 ####LOCAL####
 library(imager)
+
 file <- system.file('extdata/magnus.jpg' ,package = 'imager')
 marvs <-load.image(file)
 plot(marvs)
-library(imager)
+
+
 file <- system.file('extdata/magnus.jpg' ,package = 'imager')
 marvs <- load.image(file)
 marvs<- resize(marvs, 250, 250)
 plot(marvs,main = "Resized")
 
 
-install.packages("magick")
 ####WEB####
+
+install.packages("magick")
+
 marv <- image_read('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vx634e-T3IbKlu3pZpuZGYV0PHl1T3KZuXO65lTAuohIpgUW')
 print(marv)
-library(magick)
-marv <- image_read('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vx634e-T3IbKlu3pZpuZGYV0PHl1T3KZuXO65lTAuohIpgUW')
-print(marv)
+
 
 
