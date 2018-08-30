3dde5d9dd057c77705fa06db9430f0c7b56f4036
marverick87 <marverick87@yahoo.com>
2018-08-30 15:40:39 +0800

    Navarro,Marverick8/28/18

--- Seatwork#2/sw2.R
+++ Seatwork#2/sw2.R
@@ -1,24 +1,19 @@
 install.packages("imager")
-
+install.packages("magick")
 library(imager)
+library(magick)
+
 file <- system.file('extdata/parrots.png' ,package = 'imager')
 marv <-load.image("C:/Users/RM A-225/Documents/R/win-library/3.5/imager/extdata/parrots.png")
 plot(marv)
 
 
 #Resize Image
-library(imager)
 file <- system.file('extdata/parrots.jpg' ,package = 'imager')
 marv <- load.image('parrots.jpg')
 marv <- resize(marv, 250, 250)
 plot(marv,main = "Resized")
 
-#ReadImageThruWeb
-install.packages("magick")
-
-library(magick)
-marv <- image_read('https://i.pinimg.com/originals/40/81/47/408147adeaffa8f3437acaf065049d31.png')
-print(marv)
 
 #grayscale
 layout(t(1:2))
@@ -31,10 +26,12 @@ grayscale(marv) %>% plot(grayscale=cscale,rescale=FALSE)
 
 
 #Invert Image
-library(imager)
-fpath <- system.file('exdata/parrots.png' , package = 'imager')
+path <- system.file('exdata/parrots.png' , package = 'imager')
 marv <- load.image("C:/Users/RM A-225/Documents/R/win-library/3.5/imager/extdata/parrots.png")
 imrotate(marv,270) %>% plot(main = "Rotating")
 
+#ReadImageThruWeb
+marv <- image_read('https://i.pinimg.com/originals/40/81/47/408147adeaffa8f3437acaf065049d31.png')
+print(marv)
 
   
