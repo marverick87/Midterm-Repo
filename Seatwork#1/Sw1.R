e82eb19fad2c3669d2685da0073930d6c978ed05
marverick87 <marverick87@yahoo.com>
2018-08-30 15:42:16 +0800

    Navarro,Marverick/8/23/18

--- Seatwork#1/Sw1.R
+++ Seatwork#1/Sw1.R
@@ -1,6 +1,6 @@
 ####LOCAL####
 library(imager)
-
+install.packages("magick")
 file <- system.file('extdata/magnus.jpg' ,package = 'imager')
 marvs <-load.image(file)
 plot(marvs)
@@ -13,9 +13,6 @@ plot(marvs,main = "Resized")
 
 
 ####WEB####
-
-install.packages("magick")
-
 marv <- image_read('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vx634e-T3IbKlu3pZpuZGYV0PHl1T3KZuXO65lTAuohIpgUW')
 print(marv)
 
